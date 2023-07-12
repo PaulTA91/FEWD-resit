5c7076655c2583211e53415991669117d01c9634
PaulTA91 <parmst202@caledonian.ac.uk>
2023-07-12 11:30:35 +0100

    Map location added and working with Leaflet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "leaflet/dist/leaflet.css";
 
 import Countries from "./Countries";
 
--- src/CityLocation.js
+++ src/CityLocation.js
@@ -1,28 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { cityLocationData } from "./CityLocationData";
+import L from "leaflet";
 
 const CityLocation = ({ city }) => {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
+  const mapRef = useRef(null);
+  const mapInstanceRef = useRef(null);
 
   useEffect(() => {
-    console.log("City Location Data:", cityLocationData);
-    console.log("City:", city);
-
     const searchCityLocation = () => {
       for (const countryData of cityLocationData) {
         const selectedCity = countryData.cities.find(
           (cityData) => cityData.city_name === city,
         );
         if (selectedCity) {
-          console.log("Match found:", selectedCity);
           setLatitude(selectedCity.lat.toString());
           setLongitude(selectedCity.lng.toString());
-          return; // Exit the loop if a match is found
+          return;
         }
       }
-
-      console.log("No match found");
       setLatitude("");
       setLongitude("");
     };
@@ -31,11 +28,21 @@ const CityLocation = ({ city }) => {
   }, [city]);
 
   useEffect(() => {
-    console.log("Latitude:", latitude);
-    console.log("Longitude:", longitude);
+    if (latitude && longitude) {
+      if (mapInstanceRef.current) {
+        mapInstanceRef.current.remove();
+      }
+      const map = L.map(mapRef.current).setView([latitude, longitude], 13);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "&copy; OpenStreetMap contributors",
+        maxZoom: 18,
+      }).addTo(map);
+      L.marker([latitude, longitude]).addTo(map);
+      mapInstanceRef.current = map;
+    }
   }, [latitude, longitude]);
 
-  return <div>City Location Component</div>;
+  return <div ref={mapRef} style={{ height: "400px" }}></div>;
 };
 
 export default CityLocation;
